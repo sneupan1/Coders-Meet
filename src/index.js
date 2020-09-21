df0239bde601e61eee3b13c16153f320f417c1a4
Saurav Neupane <srvneupane@gmail.com>
2020-09-21 11:47:08 -0500

    prepare for deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./db/db");
-
+const path = require("path");
 const app = express();
 
 //Connect Database
@@ -11,13 +11,21 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("api is running"));
-
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`SERVER STARTED ON PORT ${PORT}`);
