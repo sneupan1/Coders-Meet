9d522b70a3183665451f219d62ce38aa55303970
Saurav Neupane <srvneupane@gmail.com>
2020-09-21 12:08:42 -0500

    deployment changes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 //serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
